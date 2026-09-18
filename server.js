1c057603b538bd482b2fb323d6decb0256fb735a
Hgregori <Hgregorio@byupathway.edu>
2026-09-18 18:55:04 -0300

    LittleFIXpt2

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const router = require('./router/router.js');
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 startMongodb();
 
 app.use(express.static(__dirname));
