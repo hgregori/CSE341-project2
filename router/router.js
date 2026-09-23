f73e19b48f22111f3be7f858d38e12ac8e9cd57e
Hgregori <Hgregorio@byupathway.edu>
2026-09-23 19:03:19 -0300

    W04-PPpt1-AddingOAuth

--- router/router.js
+++ router/router.js
@@ -1,43 +1,78 @@
 // -#-#-#-#-#-#-#-#-#-#-#-#- Controlls and Libraries -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#- //
 
 const router = require('express').Router();
+const passport = require('passport');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
 const dishesController = require('../controllers/dishes.js');
-const gamesController = require('../controllers/games.js');
+const gamesController = require('../controllers/games.js');     
 const validationMethod = require('../controllers/validation.js')
+
+const authenticateController = require('../controllers/authenticate.js');
 
 // -#-#-#-#-#-#-#-#-#-#-#-#- Dishes Paths -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#- //
 
-router.get('/dishes', dishesController.getAllDishes);
+router.get('/dishes',authenticateController.isAuthenticated, dishesController.getAllDishes);
 
-router.get('/dishes/:id', dishesController.getSingleDish);
+router.get('/dishes/:id',authenticateController.isAuthenticated, dishesController.getSingleDish);
 
-router.post('/dishes', validationMethod.validationCreateDish, dishesController.createDishe);
+router.post('/dishes',authenticateController.isAuthenticated, validationMethod.validationCreateDish, dishesController.createDishe);
 
-router.put('/dishes/:id', validationMethod.validationUpdateDish, dishesController.updateDishe);
+router.put('/dishes/:id',authenticateController.isAuthenticated, validationMethod.validationUpdateDish, dishesController.updateDishe);
 
-router.delete('/dishes/:id', validationMethod.validationDeleteDish, dishesController.deleteDishe);
+router.delete('/dishes/:id',authenticateController.isAuthenticated, validationMethod.validationDeleteDish, dishesController.deleteDishe);
 
 // -#-#-#-#-#-#-#-#-#-#-#-#- Games Paths -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#- //
 
-router.get('/games', gamesController.getAllGames);
+router.get('/games',authenticateController.isAuthenticated, gamesController.getAllGames);
 
-router.get('/games/:id', gamesController.getSingleGame);
+router.get('/games/:id',authenticateController.isAuthenticated, gamesController.getSingleGame);
 
-router.post('/games', validationMethod.validationCreateGame, gamesController.createGame);
+router.post('/games',authenticateController.isAuthenticated, validationMethod.validationCreateGame, gamesController.createGame);
 
-router.put('/games/:id', validationMethod.validationUpdateGame, gamesController.updateGame);
+router.put('/games/:id',authenticateController.isAuthenticated, validationMethod.validationUpdateGame, gamesController.updateGame);
 
-router.delete('/games/:id', validationMethod.validationDeleteGame, gamesController.deleteGame);
+router.delete('/games/:id',authenticateController.isAuthenticated, validationMethod.validationDeleteGame, gamesController.deleteGame);
 
 // -#-#-#-#-#-#-#-#-#-#-#-#- Swagger Paths -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#- //
 
 router.use('/api-docs', swaggerUi.serve);
 
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+// -#-#-#-#-#-#-#-#-#-#-#-#- Authentication Paths -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#- //
+
+router.get('/login', passport.authenticate('github', (req, res) => {
+}));
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+        if (err) { 
+            return next(err);
+        }
+        req.session.user = undefined;
+        console.log('Logged out');
+        res.redirect('/');
+    });
+});
+
+router.get('/', (req, res) => 
+    {
+        res.send(req.session.user !== undefined? 'Logged in as ' + req.session.user.username: 'Logged Out')
+    }
+);
+
+router.get('/github/callback', 
+    passport.authenticate('github', 
+    { failureRedirect: '/api-docs', 
+        session: false }), 
+        (req, res) => {
+            req.session.user = req.user;
+            res.redirect('/');
+        }
+);
 
 // -#-#-#-#-#-#-#-#-#-#-#-#- Modules Export -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#- //
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,58 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const { startMongodb } = require('./config/db.config');
 const router = require('./router/router.js');
+const passport = require('passport');
+const expressSession = require('express-session');
+const cors = require('cors');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 app.use(express.json())
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});
+app
+    .use(expressSession({
+        secret: process.env.SESSION_SECRET || "secret",
+        resave: false,
+        saveUninitialized: true
+    }))
+
+    .use(passport.initialize())
+
+    .use(passport.session())
+
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+        );
+        res.setHeader('Access-Control-Allow-Methods', 
+            'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        next();
+    });
 
 startMongodb();
+app.use(cors({ methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"] }));
+app.use(cors({ origin: '*' }));
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL,
+},
+function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
 
 app.use(express.static(__dirname));
 
